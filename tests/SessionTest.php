3282769412deeeab36bbf6ed573b8d6c616a9343
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2023-01-18 20:50:26 +0100

    Add beforeEach and afterEach to SessionTest

--- tests/SessionTest.php
+++ tests/SessionTest.php
@@ -5,54 +5,59 @@ declare(strict_types=1);
 use Conia\Session\OutOfBoundsException;
 use Conia\Session\Session;
 
-test('Session set/has/get', function () {
-    $session = new Session();
-    $session->set('Chuck', 'Schuldiner');
+beforeEach(function () {
+    $this->session = new Session();
+    $this->session->start();
+});
 
-    expect($session->has('Chuck'))->toBe(true);
-    expect($session->get('Chuck'))->toBe('Schuldiner');
+
+afterEach(function () {
+    $this->session->forget();
+});
+
+
+test('Session set/has/get', function () {
+    $this->session->set('Chuck', 'Schuldiner');
+
+    expect($this->session->has('Chuck'))->toBe(true);
+    expect($this->session->get('Chuck'))->toBe('Schuldiner');
 });
 
 
 test('Session unset', function () {
-    $session = new Session();
-    $session->set('Chuck', 'Schuldiner');
+    $this->session->set('Chuck', 'Schuldiner');
 
-    expect($session->get('Chuck'))->toBe('Schuldiner');
-    expect($session->has('Chuck'))->toBe(true);
+    expect($this->session->get('Chuck'))->toBe('Schuldiner');
+    expect($this->session->has('Chuck'))->toBe(true);
 
-    $session->unset('Chuck');
+    $this->session->unset('Chuck');
 
-    expect($session->get('Chuck', null))->toBe(null);
-    expect($session->has('Chuck'))->toBe(false);
+    expect($this->session->get('Chuck', null))->toBe(null);
+    expect($this->session->has('Chuck'))->toBe(false);
 });
 
 
 test('Session throws when missing', function () {
-    $session = new Session();
-    $session->get('To exist in this world may be a mistake');
+    $this->session->get('To exist in this world may be a mistake');
 })->throws(OutOfBoundsException::class, 'To exist in this world may be a mistake');
 
 
 test('Session get default', function () {
-    $session = new Session();
-    expect($session->get('Rick', 'Rozz'))->toBe('Rozz');
+    expect($this->session->get('Rick', 'Rozz'))->toBe('Rozz');
 });
 
 
 test('Flash messages all', function () {
-    $session = new Session();
+    expect($this->session->hasFlashes())->toBe(false);
 
-    expect($session->hasFlashes())->toBe(false);
+    $this->session->flash('Your existence is a script');
+    $this->session->flash('Time is a thing we must accept', 'error');
 
-    $session->flash('Your existence is a script');
-    $session->flash('Time is a thing we must accept', 'error');
+    expect($this->session->hasFlashes())->toBe(true);
+    expect($this->session->hasFlashes('error'))->toBe(true);
+    expect($this->session->hasFlashes('info'))->toBe(false);
 
-    expect($session->hasFlashes())->toBe(true);
-    expect($session->hasFlashes('error'))->toBe(true);
-    expect($session->hasFlashes('info'))->toBe(false);
-
-    $flashes = $session->popFlashes();
+    $flashes = $this->session->popFlashes();
     expect(count($flashes))->toBe(2);
     expect($flashes[0]['queue'])->toBe('default');
     expect($flashes[1]['queue'])->toBe('error');
@@ -60,48 +65,42 @@ test('Flash messages all', function () {
 
 
 test('Flash messages queue', function () {
-    $session = new Session();
+    expect($this->session->hasFlashes())->toBe(false);
 
-    expect($session->hasFlashes())->toBe(false);
+    $this->session->flash('Your existence is a script');
+    $this->session->flash('Time is a thing we must accept', 'error');
 
-    $session->flash('Your existence is a script');
-    $session->flash('Time is a thing we must accept', 'error');
-
-    $flashes = $session->popFlashes('error');
+    $flashes = $this->session->popFlashes('error');
     expect(count($flashes))->toBe(1);
     expect($flashes[0]['queue'])->toBe('error');
 
-    $flashes = $session->popFlashes();
+    $flashes = $this->session->popFlashes();
     expect(count($flashes))->toBe(1);
     expect($flashes[0]['queue'])->toBe('default');
 });
 
 
 test('Remember URI', function () {
-    $session = new Session();
-    $session->rememberUri('https://www.example.com/albums');
+    $this->session->rememberUri('https://www.example.com/albums');
 
-    expect($session->rememberedUri())->toBe('https://www.example.com/albums');
-    expect($session->rememberedUri())->toBe('/');
+    expect($this->session->rememberedUri())->toBe('https://www.example.com/albums');
+    expect($this->session->rememberedUri())->toBe('/');
 
     // Test to return '/' when expired
-    $session->rememberUri('https://www.example.com/albums', -3600);
-    expect($session->rememberedUri())->toBe('/');
+    $this->session->rememberUri('https://www.example.com/albums', -3600);
+    expect($this->session->rememberedUri())->toBe('/');
 });
 
 
-test('Session run start/forget/regenerate', function () {
+test('Session run start/forget', function () {
     // Merely runs the code without effect.
     // Can't be tested properly.
-    $session = new Session();
-    $session->start();
-    $session->set('Chuck', 'Schuldiner');
+    $this->session->start();
+    $this->session->set('Chuck', 'Schuldiner');
 
-    expect($session->get('Chuck'))->toBe('Schuldiner');
+    expect($this->session->get('Chuck'))->toBe('Schuldiner');
 
-    $session->forget();
+    $this->session->forget();
 
-    expect($session->has('Chuck'))->toBe(false);
-
-    $session->regenerate();
+    expect($this->session->has('Chuck'))->toBe(false);
 });
